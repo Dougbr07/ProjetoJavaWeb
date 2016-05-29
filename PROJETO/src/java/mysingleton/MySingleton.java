e2c63cfdb7a4dd3622e8142a4a3d9b3df9912eb8
Douglas Oliveira <douglas.tom@hotmail.com>
2016-05-29 17:37:04 -0300

    Update MySingleton.java

--- PROJETO/src/java/mysingleton/MySingleton.java
+++ PROJETO/src/java/mysingleton/MySingleton.java
@@ -1,62 +1,48 @@
-package Servlets;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package mysingleton;
 
-import Bean.Pessoa;
-import DAO.JdbcPessoaDao;
-import java.io.IOException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author Douglas
  */
-@WebServlet(urlPatterns = {"/ServletCadastro"})
-public class ServletCadastro extends HttpServlet {
+public class MySingleton {
 
-
- 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    private static Connection connection = null;
+    private MySingleton(){}
     
-       Pessoa novaPessoa = new Pessoa();
-       novaPessoa.setNome(request.getParameter("nome"));
-       novaPessoa.setCpf(request.getParameter("cpf"));
-       novaPessoa.setUsuario(request.getParameter("usuario"));
-       novaPessoa.setSenha(request.getParameter("senha"));
-       novaPessoa.setRg(request.getParameter("rg"));
-       novaPessoa.setEmail(request.getParameter("email"));
-       novaPessoa.setEndereco(request.getParameter("endereco"));
-        novaPessoa.setTelefone(request.getParameter("telefone"));
-       
-       JdbcPessoaDao pessoa = new JdbcPessoaDao();
-       boolean validarCadastro = pessoa.insert(novaPessoa);
-       int validar = 2;
-       
-       if(validarCadastro){
-        RequestDispatcher r = request.getRequestDispatcher( "exibir.jsp" );
-        request.setAttribute("pessoa", novaPessoa);
-        r.forward( request, response );  
-       }else{
-        RequestDispatcher r = request.getRequestDispatcher( "index.jsp" );
-        request.setAttribute("validar", validar);
-        r.forward( request, response );
-       
-       
-       }
-        
+    public static Connection getConnection(){
+        if(!(connection instanceof Connection)){
+            try{
+            
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/projetoweb", "postgres", "isabelle");
+                
+            
+            }catch(SQLException | ClassNotFoundException e){
+                e.printStackTrace();
+                 System.out.println("Oops! Ocorreu um erro inesperado!");
+            }
+          
+        }
+    return connection;
     }
-
-
-
+    
+    public static void closeConnection() throws SQLException{
+        if(connection instanceof Connection){
+            
+                connection.close();
+                
+        }
+    
+    
+    }
+    
 }
